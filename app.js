8836f100e6acef7b9e0cbc781f3b7bb7eb6257b6
Muhammad Jawwad <themdjawwad@gmail.com>
2025-04-18 19:02:49 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import mongoose from "mongoose";
 import userRoute from "./Route/user.route.js";
 import productRoute from "./Route/product.route.js";
 import env from "dotenv";
+import cors from "cors";
 env.config();
 
 const PORT = process.env.PORT || 2090;
 const DBURL = process.env.DB;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/", userRoute);
 app.use("/product", productRoute);
